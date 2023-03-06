f54eb904b720042982f07076c403b576ef22aebf
zhouwenyang <1052883320@qq.com>
2023-03-06 14:28:07 +0800

    ForDemo02

--- src/main/java/xunhuan/ForDemo01.java
+++ src/main/java/xunhuan/ForDemo01.java
@@ -1,2 +1,19 @@
-package xunhuan;public class ForDemo01 {
+package xunhuan;
+
+import java.util.Scanner;
+
+/*
+for循环断线重连
+ */
+public class ForDemo01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入get开始进行重新连接");
+        String s = sc.nextLine();
+        if (s.equals("get")){
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("正在进行第"+i+"次重新连接");
+            }
+        }
+    }
 }
--- src/main/java/xunhuan/ForDemo02.java
+++ src/main/java/xunhuan/ForDemo02.java
@@ -1,2 +1,27 @@
-package xunhuan;public class ForDemo02 {
+package xunhuan;
+
+import java.util.Scanner;
+
+public class ForDemo02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入开始数");
+        int start = sc.nextInt();
+        System.out.println("请输入结束数");
+        int end = sc.nextInt();
+
+        int count = 0;
+
+        //找出能被3，5整除的数找出能被3，5整除的数
+        System.out.println("能被3，5整除的数");
+        for (int i = start; i <= end; i++) {
+            if (i % 3==0 && i%5==0){
+                System.out.println(i);
+                count++;
+            }
+        }
+
+        System.out.println("这样的数一共有"+count+"个");
+
+    }
 }
