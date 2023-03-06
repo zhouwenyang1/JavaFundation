ed89aab512b20bf409e0b54ce277f5ed20964be2
zhouwenyang <1052883320@qq.com>
2023-03-06 15:14:38 +0800

    数组前后交换

---
+++ src/main/java/array/ArrayDemo06.java
@@ -0,0 +1,20 @@
+package array;
+
+import java.util.Arrays;
+
+/*
+前后调转
+ */
+public class ArrayDemo06 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        for (int i = 0,j=arr.length-1; i < j; i++,j--) {
+            int temp = arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
